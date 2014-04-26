f1fff34760e399c8d49aa6711a1872c78ae94a8f
User Name <user@server>
2014-04-25 22:15:56 -0700

    Expanded content and templates.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
+app.config.from_object('config')
 
-from words import get_words
+from words import get_room, get_words
 
 @app.route('/')
 def index():
@@ -11,7 +12,17 @@ def index():
 
 @app.route('/play')
 def play():
-	return render_template('play.html', **get_words())
+	if not 'level' in session:
+		session['level'] = 0
+		return render_template('first.html')
+
+	session['level'] += 1
+	return render_template(get_room(session['level']), **get_words(session['level']))
+
+@app.route('/quit')
+def quit():
+	session.pop('level', None)
+	return redirect(url_for('index'))
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
--- words.py
+++ words.py
@@ -1,20 +1,29 @@
 import random
 
-animals = ['cat', 'dog', 'sheep', 'weasel', 'toad', 'pangolin']
-colors = ['off-brown', 'shiny silver', 'yellowish', 'mauve', 'blood red', 'sickly green']
+dictionary = {
+	'animals': ['cat', 'dog', 'sheep', 'weasel', 'toad', 'pangolin'],
+	'colors': ['off-brown', 'shiny silver', 'yellowish', 'mauve', 'blood red', 'sickly green'],
+	'adjectives': ['happy', 'silly', 'fluffy'],
+}
+
 titles = ['a room', 'a {color} room', 'a large room', 'the last room']
 descriptions = [
 	"This is a terrible excuse for a room description. You can't see a thing.",
-	"The walls are covered in fresh {color} paint. You really hope that's paint.",
+	"The walls are covered in fresh {color} paint.",
 	"You feel yourself throwing off your surface emotions and sinking deeper into your true self.",
-	"Out of the corner of your eye, you glimpse a pile of {animal}s. You turn around to find the room empty except for a single piece of paper with a crude {animal} drawn on it in {color} crayon.",
-	"Your vision blurs as you try to examine the room. Questions swirl through your thoughts. Who are you? Why are you here? Was that a {color} {animal}? What are you digging for? Why can't you see clearly?"
-]
+	"Out of the corner of your eye, you glimpse a pile of {adjective} {animal}s. You turn around to find the room empty except for a single piece of paper with a crude {animal} drawn on it in {color} crayon.",
+	"Your vision blurs as you try to examine the room. Questions swirl through your thoughts. Who are you? Why are you here? Was that a {color} {animal}? What are you digging for? Why do you feel so {adjective}? Why can't you see clearly?",
+	"You see a row of {level} short lines drawn on the wall in {color} crayon. You don't think they were there a minute ago. You don't see any crayons in the room, either."
+	]
 
-def get_words():
-	words = {}
-	words['color'] = random.choice(colors)
-	words['animal'] = random.choice(animals)
+def get_words(level=0):
+	words = {word[:-1]: random.choice(dictionary[word]) for word in dictionary}
+	words['level'] = level
 	words['title'] = random.choice(titles).format(**words).title()
 	words['description'] = random.choice(descriptions).format(**words)
 	return words
+
+def get_room(level=0):
+	if level == 13:
+		return 'unlucky.html'
+	return 'play.html'
