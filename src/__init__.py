3e6d81b3cc530f489f5115b74f8ff86853c0ba99
User Name <user@server>
2014-04-28 14:12:40 -0700

    Added version notes to home page.

--- src/__init__.py
+++ src/__init__.py
@@ -9,13 +9,24 @@ def index():
 	return render_template('jam_play.html',
 		title='LD29 - Beneath the Surface',
 		links=['<a href="{}">play compo version</a> (finished)'.format(url_for('compo_index')),
-			'<a href="{}">play jam version</a> (in progress, better graphics)'.format(url_for('jam_index')),
+			'<a href="{}">play jam version</a> (in progress)'.format(url_for('jam_index')),
 			'<a href="http://github.com/DarkArtsAndSciences/ld29-beneath-the-surface">source code<a> (GitHub)'],
 		description="""
 <div class="bg" style="min-height: 100px">
 <p>I made a game for Ludum Dare 29 about cheating at making a game for Ludum Dare 29. I don't think I survived.</p>
 
-<p>Controls: Mostly just clicking links; if you can read this, you can play the game. However, to win the game you'll have to cheat, and to cheat, you'll have to&lt;--ERROR: could not parse 'help_cheat.py'<br/>Try your browser's View Source for more information.--></p>
+<p><b>How to Play:</b></p>
+<p>The correct way to play is to click the links.</p>
+<p>To win the game you'll have to cheat, and to cheat, you'll have to&lt;--ERROR: could not parse 'help_cheat.py'<br/>Try your browser's View Source for more information.--></p>
 <!-- 'help_cheat.py': type cheat codes directly into your browser's address bar -->
+<p><b>Jam Version Updates:</b></p>
+
+<p>Constant story editing, and (in reverse chronological order):</p>
+
+<ul>
+<li>A special background image beneath certain parts of the story text.</li>
+<li>Favicons!</li>
+<li>Added photographic proof: the story is true, at least up to "I needed a whiteboard and some other stuff that was on sale".</li>
+</ul>
 </div>
 """)
