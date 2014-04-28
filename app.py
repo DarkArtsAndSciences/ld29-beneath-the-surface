0fabb9399c020e6a5e558be619a41dae4157e4b8
User Name <user@server>
2014-04-27 17:48:17 -0700

    Final update before end of compo (48hr).

--- app.py
+++ app.py
@@ -8,20 +8,18 @@ from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
 app.config.from_object('config')
 
-from words import get_words
-
 @app.route('/')
 def index():
 	if not 'mode' in session:
 		session['mode'] = 'start'
 
 	words = {
-		'start': ['', 'someone', 'change', 'to stop', 'depend'],
-		'surface': ['', 'someone', 'end', 'to stop', 'depend'],
-		'beneath': ['', 'something', 'end', 'to stop', 'depend'],
-		'fail': ['', 'something', 'end', '', 'depend'],
-		'multi': [', yet', 'something', 'end', '', 'end'],
-		'final': [', yet', 'something', 'end', '', 'end']
+		'start': ['up', '', 'someone', 'change', 'win', 'to stop', 'depend'],
+		'surface': ['up', '', 'someone', 'change', 'cheat', 'to stop', 'depend'],
+		'beneath': ['down', '', 'something', 'end', 'die', '', 'depend'],
+		'fail': ['down', '', 'something', 'end', 'cheat', 'many', 'depend'],
+		'multi': ['up', ', yet', 'something', 'end', 'die', 'all the', 'end'],
+		'final': ['down', ', yet', 'something', 'end', 'win', '', 'end']
 	}[session['mode']]
 
 	links = {
@@ -45,12 +43,12 @@ def index():
 'Be careful. I think...I might have made it angry.',
 "I don't want that."],
 		description="""
-<center><img src='/static/face.gif'></center>
+<center><img src='/static/face_{}.gif'></center>
 <p>This isn't really a game. It's just the only way I have left to communicate.</p>
 <p>I can't exactly say I wrote it alone, either. I don't mean there are any other people involved{}. It's just...more like something that happened to me, something {} did <b>to</b> me, than something <b>I</b> did.</p>
 <!-- Please don't let this be something I did. -->
 <p>I <b>can</b> say it all started less than 48 hours ago. It doesn't take more than a weekend for your entire life to {}.</p>
-<p>Anyway, I'm not posting because I want to win, or for fame, or even just for fun.</p>
+<p>Anyway, I'm not posting this because I want to {}, or for fame, or even just for fun.</p>
 <!-- I don't want that. -->
 <!-- I don't want that. -->
 <!-- I don't want that. -->
@@ -82,10 +80,11 @@ def story():
 <p>So I decided to head out to my local office suppy store to pick up a whiteboard (that's where ideas come from, right?) and then realized I didn't have enough markers. Also they had bracelet USB drives on sale. And a glass standing desk whiteboard in a block of cork for sticking things to with sharp pins! I bet everyone else is making their graphics in Photoshop or something equally commonplace.</p>
 <p>[placeholder: photo]</p>
 <p>Maybe I'm too vulnerable to temptation.
-<p>Anyway, after leaning the larger whiteboard against the wall and drawing a rainbow-scented pile of lack of artistic talent on it, I decided to try out a new USB drive.
+<p>Anyway, after leaning the larger whiteboard against the wall and drawing a rainbow-scented pile of lack of artistic talent on it, I decided to try out my new USB drive.
 <p>Maybe I'm just biased by what happened later, but I could swear I had a feeling that the idea I needed for my game was on that drive somehow. I mean, I know drives sometimes come with free terrible MP3s and stuff like that, and you can find inspiration in anything, but this was something more. Like a cold, solid certainty, like the way I felt when I signed my taxes two weeks ago, something final yet endless. Thanks for the support, we'll be back for more next year. I was that sure, that the idea was in there, that it was something I could use and reuse.</p>
 <p>I wish I'd spent more time trying to come up with an idea of my own.</p>
 <p>The drive wouldn't go into the USB port at first. I flipped it a few times, still no luck. Finally I blew into it, as if it were an old NES cartridge, and a scrap of paper fell out, probably the "inspected by" sticker. Well, thank you for my protection. Without the paper in the way, the drive clicked neatly into the port and showed up on my computer a few seconds later.</p>
+<center><img src='/static/surface.png'></center>
 <p>It was empty.</p>
 <p>Not even a readme file helpfully explaining how to install USB drivers so you can mount the drive and read the readme.</p>
 <p>I hadn't expected working game code or anything like that. But an entertainingly terrible MP3 would have been nice. Even a wallpaper with a giant pixelated version of the corporate logo would have been something. I felt a rush of disappointment, then anger, far stronger than the situation deserved.</p>
@@ -98,12 +97,12 @@ def story():
 <p>I spent the rest of the day trying to come up with a game idea without success.</p>
 <p>I didn't even sleep well. I kept having nightmares about a face staring at me with empty eyes, flipping itself over and over and always looking like it was still upside down, mouth open in silent mocking laughter.</p>
 <p>When I opened my laptop the next morning, all my windows had been closed -- as dramatic as it looks, slamming the lid down shouldn't do anything except put it to sleep -- except for the one showing my new USB drive. It was named BENEATH now, and contained several code and text files.</p>
-<p>[screenshot of final code in Finder]</p>
+<center><img src='/static/beneath.png'></center>
 <p>Curious, I opened a few files. It looked like some sort of web app, but I couldn't figure out what it did, and large sections of the code were commented out entirely.</p>
 <p>It took some Serious Difficult Code Hacking, but eventually I figured out how to start the server,</p>
 <p>[screenshot of ">python app.py"]</p>
 <p>and use the app.</p>
-<p>[screenshot of localhost in Safari; there's already a saved game named BENEATH]</p>
+<p>[screenshot of app in Safari; there's already a saved game named BENEATH]</p>
 <p>It's a game? Perfect! Maybe I'll steal it. I mean use it. This code wasn't here yesterday, so it's like it was written during the competition, right? Maybe I wrote it myself while sleepwalking last night, I don't remember and you can't prove me wrong.</p>
 <p>And then I noticed that the game's logo was the face from my nightmares.</p>
 """},
@@ -198,13 +197,19 @@ def walkthrough():
 		'fail': {'title': 'Walkthrough: Failure', 'description': """
 <p>I don't know what's wrong with this game. I'm not even dying anymore, it just cuts straight to my corpse with the evil NPCs standing over it. Staring.</p>
 <p>Why can't I stop playing?</p>
-<p>I wish there was a way to enable multiple save files. Maybe then it would let me start over.</p>
+<p>I wish there was a way to enable multiple save files. Maybe then it would let me start over. I've tried a few <a href='/cheat/code'>cheat codes</a>, but I couldn't figure out the right one.</p>
+<p>I wish I could start over.</p>
+<p>&nbsp;</p>
+<p>Oh, I found the piece of paper that was stuck in the drive at first, remember? The one I thought was an inspection sticker?</p>
+<p>It's a piece of old, thin paper, folded up with a black ink symbol on the inside and a red X on the outside. I don't think I have to tell you what the symbol is. [photo]</p>
 """},
 		'multi': {'title': 'Walkthrough: The Second Save', 'description': """
-<p>Hint: try getting yourself killed!</p>
+<p>Obviously Bad Hint from the Game Pretending to Be Me: try getting yourself killed!</p>
 """},
 		'final': {'title': 'Walkthrough: The Final Save', 'description': """
-<p>Hint: try getting yourself killed again!</p>
+<p>Okay, I've finally got this game figured out. There's no way to win from inside the game. You have to give it what it wants, <b>outside</b> of the game.</p>
+<p>What does it want? It won't tell me. It just says it's not for me know, that it was my place to write the walkthrough that would guide the winner, but not to be the winner myself.</p>
+<p class='smalltext'>But if I had to guess, I'd say that, like all of its kind, it just wants to spread the infection.<p>
 """}}
 
 	page = pages[session['mode']]
@@ -216,16 +221,17 @@ def walkthrough():
 def play():
 	if 'mode' in session:
 		if session['mode'] == 'surface':
-			return inflectr.inflect(render_template('play.html', title='The Surface', links=['<a href="/play">continue</a>', '<a href="/">return to main menu</a>'], description="""
-<p>You are in a small, cheery house. The walls are off-white with white trim. The front door is locked. Your TV and computer are in the living room. Softly carpeted stairs lead up to your bedroom.</p>
-<p>You may turn the lights on and off. [links]</p>
+			return inflectr.inflect(render_template('play.html', title='The Surface', links=['<a href="/">return to main menu</a>'], description="""
+<p>You are in a small, cheery house.The walls are off-white with white trim. The front door is locked. Your TV and computer are in the living room. Softly carpeted stairs lead up to your bedroom.</p>
+<p>The local NPCs are smiling and friendly.</p>
+<p>You may turn the lights on and off.</p>
 """))
 
 		if session['mode'] == 'beneath':
-			return inflectr.inflect(render_template('play.html', title='Beneath the Surface', links=['<a href="/play">continue</a>', '<a href="/die">die</a>'], description="""
+			return inflectr.inflect(render_template('play.html', title='Beneath the Surface', links=['<a href="/play">look around</a>', '<a href="/die">die</a>'], description="""
 <p>You are in a small, dark house. Brownish paint peels off in long strips from the decaying walls. The front door lock is rusted shut. The living room has been looted of all valuables that weren't nailed down, all valuables that were nailed down, and the nails themselves. Metal prices must be up again.</p>
 <p>The stairway leading down to your bedroom looks wobbly but usable, as long as you're up to date on your tetanus shots.</p>
-<p>You may turn the lights on and off. [links, but they don't work]</p>
+<p>You may turn the lights on and off, but the bulbs were stolen years ago.</p>
 """))
 
 	return redirect(url_for('index'))
@@ -255,8 +261,8 @@ def dead():
 		title='You have died',
 		links=['<a href="/">back to main menu</a>'],
 		description="""
-<p>[short death scene]</p>
-		"""))
+<p>[image of your dead avatar. a local NPC smiles sadly]</p>
+"""))
 
 @app.route('/fail')
 def fail():
@@ -265,36 +271,36 @@ def fail():
 		title='You have failed',
 		links=['<a href="/">I am a failure.</a>'],
 		description="""
-<p>[an unskippable creepy cutscene of NPCs laughing at your corpse]</p>
+<p>[an unskippable cutscene of NPCs laughing at your corpse]</p>
 		"""))
 
 @app.route('/cheat/<code>')
 def cheat(code):
-	if code == 'clear':
+	if code in ['clear', 'restart', 'start-over']:
 		session.clear()
-		page = {'title': 'CLEAR', 'description': """
-<p>You use the cheat code CLEAR.</p>
+		page = {'description': """
+<p>You use the cheat code {code}.</p>
 <p>You feel completely free.</p>
-"""}
+""".format(code.upper())}
 
-	elif code == 'multi':
+	elif code in ['multi', 'multiple', 'many', 'files', 'enable-multiple-files']:
 		session['mode'] = 'multi'
-		page = {'title': 'MULTI', 'description': """
-<p>You use the cheat code MULTI.</p>
+		page = {'description': """
+<p>You use the cheat code {}.</p>
 <p>Your vision doubles for a moment.</p>
 <p>You feel a sudden rush of paranoia. What if someone saw you cheat? You don't want that.</p>
 <p>You hope the game isn't angry with you.</p>
-"""}
+""".format(code.upper())}
 
-	elif code == 'quit' and session['mode'] == 'surface':
+	elif code in ['quit', 'die'] and session['mode'] == 'surface':
 		session['mode'] = 'beneath'
 		session['deaths'] = 0
-		page = {'title': 'QUIT', 'description': """
-<p>You use the cheat code QUIT.</p>
+		page = {'description': """
+<p>You use the cheat code {}.</p>
 <p>You feel strangely empty.</p>
 <p>You feel a sudden rush of paranoia. What if someone saw you cheat? You don't want that.</p>
 <p>You hope this doesn't cause any weird glitches in the game.</p>
-"""}
+""".format(code.upper())}
 
 	else:
 		page = {'description': """
@@ -311,54 +317,57 @@ def cheat(code):
 	return inflectr.inflect(render_template('play.html', **page))
 
 @app.route('/play/<filename>')
-def play_filename(filename):
+@app.route('/play/<filename>/<room>')
+def play_filename(filename, room=None):
 	if filename == 'YOU_FAILED':
-		return inflectr.inflect(render_template('play.html',
-			title='YOU HAVE FAILED AGAIN',
-			links=['<a href="/">I am a repeated failure.</a>'],
-			description="""
-<p>[an unskippable creepy cutscene of NPCs laughing at your corpse, last frame only]</p>
-			"""))
-
-	if filename == 'BENEATH':
-		return inflectr.inflect(render_template('play.html',
-			title='Far Beneath the Surface',
-			links=['<a href="/play/BENEATH">continue</a>', '<a href="/die">die</a>'],
-			description="""
-<p>You are in a small, dark house. Brownish paint peels off in long strips from the decaying walls. The front door lock is rusted shut. The living room has been looted of all valuables that weren't nailed down, all valuables that were nailed down, and the nails themselves. Metal prices must be up again.</p>
-<p>The stairway leading down to your bedroom looks wobbly but usable, as long as you're up to date on your tetanus shots.</p>
-<p>You may turn the lights on and off. [links, but they don't work]</p>
+		return inflectr.inflect(render_template('play.html', title='You have failed again', links=['<a href="/">I am a repeated failure. I shall return to the main menu in shame.</a>'], description="""
+<center><img src='/static/face_down.gif'></center>
+<p>[image: the NPCs continue silently staring at your corpse]</p>
 """))
 
-	if filename == 'DONT_FAIL_AGAIN':
-		return inflectr.inflect(render_template('play.html',
-			title='Failure is Not an Option',
-			links=['<a href="/">vote</a>', '<a href="/">THE END</a>', '<a href="/cheat/clear">start over</a>'],
-			description="""
-<p>Vote for this game or it will eat your soul.</p>
-<p>To replay,
-			"""))
+	if filename == 'BENEATH':
+		if not room:
+			return inflectr.inflect(render_template('play.html', title='Far Beneath the Surface', description="""
+<p>You are in a small, dark house. Brownish paint peels off in long strips from the decaying walls. The front door lock is rusted shut. The living room has been looted of all valuables that weren't nailed down, all valuables that were nailed down, the <a href='/play/BENEATH/nails'>nails</a> themselves, and most of the floor.</p>
+<p>The <a href='/play/BENEATH/stairway'>stairway</a> leading down to your bedroom looks wobbly but usable, as long as you're up to date on your tetanus shots.</p>
+<p>You may turn the lights on and off, but the bulbs were stolen decades ago.</p>
+"""))
 
-	return inflectr.inflect(render_template('play.html',
-		title='Bad Filename',
-		links=['<a href="/">back to main menu</a>'],
-		description="""
+		rooms = {
+			'nails': {'title': 'A Rusty Nail', 'links': ['<a href="/play/BENEATH">continue</a>'], 'description': """
+<p>You find a single rusty nail that all the previous scavengers somehow missed.</p>
+<p>It is sharp and rusty and might kill you instantly.</p>
+<p>You don't want that.</p>
+"""},
+			'stairway': {'title': 'A Wobbly Stairway', 'links': ['<a href="/play/BENEATH">Yeah, right. Let me into my room already.</a>', '<a href="/die">It\'s what I deserve.</a>'], 'description': """
+<p>You fall down the stairs and die.</p>
+"""}}
+		page = rooms[room]
+		return inflectr.inflect(render_template('play.html', **page))
+
+	if filename == 'DONT_FAIL_AGAIN':
+		return inflectr.inflect(render_template('play.html', title='Failure is Not an Option', links=['<a href="/">vote</a>', '<a href="/">return to main menu</a>', '<a href="/cheat/clear">start over</a>'], description="""
+<p>Vote for this game or it will eat your soul.</p>
+<p class='bigtext'>THE END</p>
+"""))
+
+	return inflectr.inflect(render_template('play.html', title='Bad Filename', links=['<a href="/">back to main menu</a>'], description="""
 <p>There is no save file named {filename}.</p>
 <p>&nbsp;</p>
 <p>Are you trying to cheat?</p>
 <p>I hope you're not trying to cheat. That might make the game angry.</p>
 <p>You don't want that.</p>
 <p style='smalltext'>also, the correct URL is /cheat/&lt;code&gt;</p>
-			"""))
+"""))
 
 @app.route('/final')
 def final():
 	return inflectr.inflect(render_template('play.html',
-		title='The Final Scene',
+		title='The Warning',
 		links=['<a href="/">back to main menu</a>'],
 		description="""
-<p>The Final Scene. Threats before enabling DONT_FAIL_AGAIN.</p>
-		"""))
+<p>[the game overtly threatens you unless it gets what it wants, but doesn't tell you what it wants]</p>
+"""))
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
