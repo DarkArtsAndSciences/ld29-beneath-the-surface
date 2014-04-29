070fe68507fdd39402ac7817d5d56805932e3f8f
User Name <user@server>
2014-04-29 03:02:54 -0700

    Dual version of logo, regular and wallpaper.

--- src/__init__.py
+++ src/__init__.py
@@ -13,12 +13,14 @@ def index():
 			'play post-competition version (coming "soon")'.format(url_for('jam_index')),
 			'<a href="http://github.com/DarkArtsAndSciences/ld29-beneath-the-surface">source code<a> (GitHub)'],
 		description="""
-<div class="bg" style="min-height: 100px">
+<div style="min-height: 100px">
 <p>I made a game for Ludum Dare 29 about cheating at making a game for Ludum Dare 29. I don't think I survived.</p>
+
+<center><img src="/static/post/you_dont_want_that.gif"></center>
 
 <p><b>How to Play:</b></p>
 <p>The correct way to play is to click the links.</p>
-<p>To win the game you'll have to cheat, and to cheat, you'll have to&lt;--ERROR: could not parse 'help_cheat.py'<br/>Try your browser's View Source for more information.--></p>
+<p class="bg">To win the game you'll have to cheat, and to cheat, you'll have to&lt;--ERROR: could not parse 'help_cheat.py'<br/>Try your browser's View Source for more information.--></p>
 <!-- 'help_cheat.py': type cheat codes directly into your browser's address bar -->
 <p><b>Jam Version Updates:</b></p>
 
@@ -30,6 +32,11 @@ def index():
 <li>A special background image beneath certain parts of the story text.</li>
 <li>Favicons.</li>
 <li>Added photographic proof: the story is true, at least up to "I needed a whiteboard and some other stuff that was on sale".</li>
+</ul>
+
+<p><b>Post Competition Updates:</b></p>
+<ul>
+<li>Dual version of logo and better <a href="/static/post/you_dont_want_that_1920x1080.gif">wallpaper</a>.</li>
 </ul>
 </div>
 """)
